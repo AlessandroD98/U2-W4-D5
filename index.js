e6ed655bab476476b2c3027ffd663256b5bc0d1f
AlessandroD98 <alessandro.comunicazioni@gmali.com>
2023-01-14 22:01:20 +0100

    parte-js-finita

--- index.js
+++ index.js
@@ -11,3 +11,21 @@ window.onscroll = () => {
     li.classList.remove("NewLi");
   }
 };
+setInterval(DisappierM, 40);
+function DisappierM() {
+  let g = document.querySelectorAll("svg g[stroke-linecap= 'butt']");
+  let randg = g[Math.floor(Math.random() * g.length)];
+  randg.style.transition = "1.1s";
+  if (randg.getAttribute("opacity") == 1) {
+    randg.setAttribute("opacity", "0");
+  } else {
+    randg.setAttribute("opacity", "1");
+  }
+}
+
+// for (let i = 0; i < g.length; i++) {
+//   const element = g[i];
+//   if (element.getAttribute("opacity") == 0) {
+//     element.setAttribute("opacity", "1");
+//   }
+// }
